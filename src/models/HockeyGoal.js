a6e409c65052b731983d87487d2411eacef3c8c1
SikoraKam <sikorkam3@gmial.com>
2020-05-10 11:05:03 +0200

    changes after pull request: calculate goal points in seperate function

--- src/models/HockeyGoal.js
+++ src/models/HockeyGoal.js
@@ -3,26 +3,29 @@ import GameObject from './GameObject';
 export default class HockeyGoal extends GameObject {
   constructor(x, y, width, height) {
     super(x, y, width, height);
+    this.points = this.calculatePoints();
   }
 
   update(delta) {}
 
   render(ctx) {
+    ctx.fillStyle = 'dark';
+    ctx.beginPath();
+    ctx.moveTo(this.points[0][0], this.points[0][1]);
+
+    for (let i = 0; i < this.points.length; i++) {
+      ctx.lineTo(this.points[i][0], this.points[i][1]);
+    }
+    ctx.stroke();
+  }
+
+  calculatePoints() {
     const halfWidth = this.width / 2;
-    const points = [
+    return [
       [this.x - halfWidth, this.y],
       [this.x + halfWidth, this.y],
       [this.x + halfWidth, this.y + this.height],
       [this.x - halfWidth, this.y + this.height],
     ];
-
-    ctx.fillStyle = 'dark';
-    ctx.beginPath();
-    ctx.moveTo(points[0][0], points[0][1]);
-
-    for (let i = 0; i < points.length; i++) {
-      ctx.lineTo(points[i][0], points[i][1]);
-    }
-    ctx.stroke();
   }
 }
